681cc1134b848c4f5a85525a42997a0f5713b8db
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2022-06-24 09:49:04 +0530

    improve test coverage

--- test/unit/command-parser/stringCommandParser.test.ts
+++ test/unit/command-parser/stringCommandParser.test.ts
@@ -1,7 +1,9 @@
 import StringCommandParser from "../../../src/command-parser/stringCommandParser";
+import LeftCommand from "../../../src/command/leftCommand";
 import MoveCommand from "../../../src/command/moveCommand";
 import PlaceCommand from "../../../src/command/placeCommand";
 import ReportCommand from "../../../src/command/reportCommand";
+import RightCommand from "../../../src/command/rightCommand";
 import { Direction } from "../../../src/direction";
 import Robot from "../../../src/robot";
 
@@ -17,18 +19,19 @@ describe("test stringCommandParser", () => {
     });
 
     it("should return an array of command factory functions by splitting the the rawString by the delimiter", () => {
-      const rawCommands = "PLACE 1,2,NORTH\nMOVE\nREPORT";
+      const rawCommands = "PLACE 1,2,NORTH\nMOVE\nREPORT\nLEFT\nRIGHT";
       const parsedCommands = new StringCommandParser().parse(
         rawCommands,
         /\r?\n/
       );
-      expect(parsedCommands.length).toBe(3);
       const robot = new Robot();
-      expect(parsedCommands[0](robot)).toEqual(
+      expect(parsedCommands[0](robot)).toStrictEqual(
         new PlaceCommand(1, 2, Direction.North, robot)
       );
-      expect(parsedCommands[1](robot)).toEqual(new MoveCommand(robot));
-      expect(parsedCommands[2](robot)).toEqual(new ReportCommand(robot));
+      expect(parsedCommands[1](robot)).toStrictEqual(new MoveCommand(robot));
+      expect(parsedCommands[2](robot)).toStrictEqual(new ReportCommand(robot));
+      expect(parsedCommands[3](robot)).toStrictEqual(new LeftCommand(robot));
+      expect(parsedCommands[4](robot)).toStrictEqual(new RightCommand(robot));
     });
 
     it("should exclude invalid commands in the rawString", () => {
@@ -39,10 +42,10 @@ describe("test stringCommandParser", () => {
       );
       expect(parsedCommands.length).toBe(2);
       const robot = new Robot();
-      expect(parsedCommands[0](robot)).toEqual(
+      expect(parsedCommands[0](robot)).toStrictEqual(
         new PlaceCommand(1, 2, Direction.North, robot)
       );
-      expect(parsedCommands[1](robot)).toEqual(new ReportCommand(robot));
+      expect(parsedCommands[1](robot)).toStrictEqual(new ReportCommand(robot));
     });
   });
 });
--- test/unit/robot.test.ts
+++ test/unit/robot.test.ts
@@ -59,6 +59,18 @@ describe("test Robot", () => {
   });
 
   describe("test Robot.left", () => {
+    it("should throw an error if the robot is not placed on the table", () => {
+      expect.assertions(1);
+      const robot = new Robot();
+      try {
+        robot.left();
+      } catch (err: any) {
+        expect(err.message).toBe(
+          "Cannot execute command: LEFT because the robot has not been placed"
+        );
+      }
+    });
+
     it("should set direction to West if direction is North", () => {
       const robot = new Robot();
       robot.setIsPlaced(true); // TODO: replace with place()
@@ -93,6 +105,18 @@ describe("test Robot", () => {
   });
 
   describe("test Robot.right", () => {
+    it("should throw an error if the robot is not placed on the table", () => {
+      expect.assertions(1);
+      const robot = new Robot();
+      try {
+        robot.right();
+      } catch (err: any) {
+        expect(err.message).toBe(
+          "Cannot execute command: RIGHT because the robot has not been placed"
+        );
+      }
+    });
+
     it("should set direction to East if direction is North", () => {
       const robot = new Robot();
       robot.setIsPlaced(true);
@@ -123,6 +147,59 @@ describe("test Robot", () => {
       robot.setDirection(Direction.East);
       robot.right();
       expect(robot.getDirection()).toBe(Direction.South);
+    });
+  });
+
+  describe("test Robot.move", () => {
+    it("should throw an error if the robot is not placed on the table", () => {
+      expect.assertions(1);
+      const robot = new Robot();
+      try {
+        robot.move();
+      } catch (err: any) {
+        expect(err.message).toBe(
+          "Cannot execute command: MOVE because the robot has not been placed"
+        );
+      }
+    });
+
+    it("should throw an error if the robot is trying to move out of the table", () => {
+      expect.assertions(1);
+      const robot = new Robot();
+      robot.place(0, 4, Direction.North);
+      try {
+        robot.move();
+      } catch (err: any) {
+        expect(err.message).toBe("Position 5 is out of bounds on Y axis");
+      }
+    });
+
+    it("should increment the positionY if the robot is facing North and is not trying to move out of the table", () => {
+      const robot = new Robot();
+      robot.place(0, 0, Direction.North);
+      robot.move();
+      expect(robot.getPositionY()).toBe(1);
+    });
+
+    it("should decrement the positionY if the robot is facing South and is not trying to move out of the table", () => {
+      const robot = new Robot();
+      robot.place(0, 4, Direction.South);
+      robot.move();
+      expect(robot.getPositionY()).toBe(3);
+    });
+
+    it("should increment the positionX if the robot is facing East and is not trying to move out of the table", () => {
+      const robot = new Robot();
+      robot.place(0, 0, Direction.East);
+      robot.move();
+      expect(robot.getPositionX()).toBe(1);
+    });
+
+    it("should decrement the positionX if the robot is facing West and is not trying to move out of the table", () => {
+      const robot = new Robot();
+      robot.place(4, 0, Direction.West);
+      robot.move();
+      expect(robot.getPositionX()).toBe(3);
     });
   });
 });
